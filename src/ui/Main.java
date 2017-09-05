67b4a101a49ce6f6e36c8386bcc2fa3a83c560de
Simen Burud <sb@simen.codes>
2017-09-05 21:27:59 +0200

    Of course, we need a stupid name

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Flap Flap rEvolution");
         primaryStage.setScene(new Scene(root, Game.Companion.getWIDTH() * 2, Game.Companion.getHEIGHT()));
         primaryStage.show();
         primaryStage.setOnCloseRequest(value -> System.exit(0));
